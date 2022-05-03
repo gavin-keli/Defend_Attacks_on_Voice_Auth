2ef5bfc33a7a64d807d098d2e9d80930553ad6f0
like_gavin <gavinleeke@outlook.com>
2022-05-03 10:50:05 -0500

    guardian updates

---
+++ guardian/constants.py
@@ -0,0 +1,20 @@
+DATASET_DIR = '../data/Dataset-npy/test/'
+
+#stat_test
+#TEST_DIR = '../data/Dataset-npy_v2/test/'
+TEST_DIR = '../data/Dataset-npy/test/'
+
+#save_model.py
+DISCRIMINATOR_MODEL = '../github_data/guardian/discriminator_model/'
+
+DISCRIMINATOR_CHECKPOINT_FOLDER = '../github_data/guardian/discriminator_checkpoints/'
+
+CHECKPOINT_FOLDER = '../github_data/deep_speaker/users_1000_5/test_1'
+
+
+C_F_A = '../github_data/deep_speaker/users_1000_5/'
+#C_F_A = '../github/research/deep_speaker/data/checkpoints/users_440_TIMIT/'
+
+#CHECKPOINT_FOLDER_ARRAY = [C_F_A+'test_1', C_F_A+'test_2', C_F_A+'test_3', C_F_A+'test_4', C_F_A+'test_5', C_F_A+'test_6', C_F_A+'test_7', C_F_A+'test_8', C_F_A+'test_9', C_F_A+'test_10']
+CHECKPOINT_FOLDER_ARRAY = [C_F_A+'test_1', C_F_A+'test_1', C_F_A+'test_1', C_F_A+'test_1', C_F_A+'test_1', C_F_A+'test_1', C_F_A+'test_1', C_F_A+'test_1', C_F_A+'test_1', C_F_A+'test_1']
+
