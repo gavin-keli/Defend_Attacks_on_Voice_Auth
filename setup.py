00f376fbd1b4fe8b50c9c6a6513ee49c2e7e8dd5
like_gavin <gavinleeke@outlook.com>
2022-05-02 22:09:03 -0500

    empty folders

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+os.system('sudo apt update -y && sudo apt upgrade -y')
+os.system('sudo apt install ffmpeg -y')
+os.system('sudo apt install rename -y')
+os.system('sudo apt install gcc -y && sudo apt-get install gdb -y')
+os.system('sudo apt  install jq -y')
+
+
+
+
